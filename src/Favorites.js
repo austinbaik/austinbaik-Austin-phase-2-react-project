77c01c74d7aea0de77ff0786faa7dc3eb7d055aa
Austin <baik.austin@gmail.com>
2022-06-02 16:22:20 -0700

    created top-level app components

---
+++ src/Favorites.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Favorites(){
+
+    return(
+        <div> favorite images </div>
+    )
+}
+
+
+
+export default Favorites;
---
+++ src/Images.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Images(){
+
+    return(
+        //we will be returning cards 
+        <card>CARD</card>
+
+    
+    )
+}
+
+
+export default Images
