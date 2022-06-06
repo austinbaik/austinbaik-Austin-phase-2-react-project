2d7c1c4bb3f70cdd1f05bde80b5b78c72905b655
Austin <baik.austin@gmail.com>
2022-06-06 15:36:09 -0700

    removed infinite-scroll from code

--- src/Images.js
+++ src/Images.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 import React from 'react';
 import Cards from './Cards';
-import InfiniteScroll from "react-infinite-scroll-component";
-import axios from "axios";
 
 
 // api call to fetch data   
@@ -41,32 +39,32 @@ function Images({ updateState, imageArray }) {
     // axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/')
     // https://collectionapi.metmuseum.org/public/collection/v1/objects/436173
 
-    async function fetchMoreData(e) {
-        e.preventDefault()
-        let i = 0;
-        let tempArray = []
-        let newRemainingIDS = remainingImageIDs
-        do {
-            let ObjectID = newRemainingIDS + i
+    // async function fetchMoreData(e) {
+    //     e.preventDefault()
+    //     let i = 0;
+    //     let tempArray = []
+    //     let newRemainingIDS = remainingImageIDs
+    //     do {
+    //         let ObjectID = newRemainingIDS + i
 
-            let IDForArray = imageArray[ObjectID]
-            await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${IDForArray}`)
-                .then(response => response.json())
-                .then(response => {
-                    console.log('response', response)
-                    tempArray.push(response)
-                }),
-                i += 1;
-        } while (i < 9);
+    //         let IDForArray = imageArray[ObjectID]
+    //         await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${IDForArray}`)
+    //             .then(response => response.json())
+    //             .then(response => {
+    //                 console.log('response', response)
+    //                 tempArray.push(response)
+    //             }),
+    //             i += 1;
+    //     } while (i < 9);
 
-        setRemainingImageIDs(newRemainingIDS + 9)
-        setImageData([...imageData, ...tempArray]) //concat
-        //.push is destructive && must use spread array both times for objects inside to be one array
-        setHasMore(arrayLength - (newRemainingIDS + 9) > 0) //gives true or false by default 
-    }
+    //     setRemainingImageIDs(newRemainingIDS + 9)
+    //     setImageData([...imageData, ...tempArray]) //concat
+    //     //.push is destructive && must use spread array both times for objects inside to be one array
+    //     setHasMore(arrayLength - (newRemainingIDS + 9) > 0) //gives true or false by default 
+    // }
 
 
-    console.log("imageData", imageData)
+    // console.log("imageData", imageData)
 
     // newRemainingIDS < arrayLength ? setHasMore(true) : setHasMore(false)
     // setRemainingImageIDs(newRemainingIDS)
@@ -82,24 +80,10 @@ function Images({ updateState, imageArray }) {
     return (
 
         <>
-            {/* <button onClick={fetchMoreData}>
-                Call API
-            </button>
+         
+      <Cards imageData={imageData} />
 
-            <div>
-            <div style="height:700px;overflow:auto;">
-            </div> */}
-
-            <InfiniteScroll
-                dataLength={imageData.length}
-                next={fetchMoreData}
-                hasMore={hasMore}
-                loader={<h4>Loading...</h4>}
-            >
-                <Cards imageData={imageData} />
-            </InfiniteScroll>
-
-
+      
             {/* {imageData.map((img) => {
                     return (
                         <div>
