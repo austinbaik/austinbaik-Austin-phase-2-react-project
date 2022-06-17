be51cfc499dcf1239859939aefc492826f62adb1
Austin <baik.austin@gmail.com>
2022-06-16 17:19:47 -0700

    corrected controlled forms to allow for correct db.json entry via POST api call

---
+++ src/Card.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+
+
+function Card({ imgObj }) {
+    console.log("img", imgObj)
+
+    const [name, setName] = useState("")
+    const [comment, setComment] = useState("")
+    const [imgURL, setImgURL] = useState(imgObj.primaryImage)
+    const [imgName, setImgName] = useState(imgObj.title)
+    const [objID, setObjID] = useState(imgObj.objectID)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch("http://localhost:3000/images/", {
+
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: name,
+                comment: comment,
+                imgUrl: imgURL,
+                imgName: imgName,
+                objID: objID, 
+
+            }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                setName("")
+                setComment("")
+            })
+
+    }
+
+    function handleNameChange(e) {
+        setName(e.target.value)
+    }
+    function handleCommentChange(e) {
+        setComment(e.target.value)
+    }
+
+
+    return (
+
+        <div class="img-div" key={imgObj.objectID}>
+            <h1> {imgObj.title} </h1>
+            <h2> {imgObj.artistDisplayName} </h2>
+
+            <img class="img"
+                src={imgObj.primaryImage}
+                alt={imgObj.primaryImageSmall}
+            />
+
+            <br></br>
+
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Comment:
+                    <input type="text" onChange={handleNameChange} value={name} />
+                </label>
+                Name:
+                <input type="text" onChange={handleCommentChange} value={comment} />
+                <button type="submit">Submit</button>
+            </form>
+
+
+        </div>
+
+
+
+
+    )
+}
+
+
+export default Card;
--- src/Cards.js
+++ src/Cards.js
@@ -1,74 +1,23 @@
 import React, { useState } from "react";
 import './App.css';
-
+import Card from './Card';
 
 function Cards({ imageData }) {
 
-    const [name, setName] = useState("")
-    const [comment, setComment] = useState("")
 
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        fetch("http://localhost:3000/images?id=7", {
-
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name: name, 
-                comment: comment
-            }),
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-            })
-    }
-
-
-
-    function handleNameChange(e) {
-        setName(e.target.value)
-    }
-    function handleCommentChange(e) {
-        setComment(e.target.value)
-    }
-
-    let eachImage = imageData.map((img) => {
-        return (
-            <div class="img-div" key={img.objectID}>
-                <h1> {img.title} </h1>
-                <h2> {img.artistDisplayName} </h2>
-
-                <img class="img"
-                    src={img.primaryImage}
-                    alt={img.primaryImageSmall}
-                />
-
-                <br></br>
-
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Comment:
-                        <input type="text" onChange={handleNameChange} value={name} />
-                    </label>
-                        Name:
-                    <input type="text" onChange={handleCommentChange} value={comment} />
-                    <button type="submit">Submit</button>
-                </form>
-
-
-            </div>
-        )
-    });
 
     return (
         <>
             "Hold This Space"
             <div>
-                {eachImage}
+          {  imageData.map((img) => {
+        return (
+
+                <Card imgObj={img} />
+
+                )
+    })}
             </div>
         </>
     )
